e59da38f1884e127f249c1125a9c0707308ba87d
Zuzu-Typ <Zuzu.Typ@gmail.com>
2019-11-07 16:49:43 +0100

    Update to Version 1.1.2

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.1',
+    version='1.1.2',
 
     description='OpenGL Mathematics library for Python',
     long_description=long_description,
