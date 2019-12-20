7bb115b11afbcd1fccba79f669d9b7634f5b29e7
Zuzu-Typ <Zuzu.Typ@gmail.com>
2019-12-20 23:03:11 +0100

    Updated licenses (#51)
    * Updated licenses
    
    + Added the license information as 'license' in PyGLM
    
    ~ Switched to the unmodified MIT license in COPYING
    
    - Removed GLM_LICENSE
    
    * Updated tests
    
    added check for version and license

--- PyGLM.cpp
+++ PyGLM.cpp
@@ -1,5 +1,62 @@
 #define PyGLM_VERSION "1.1.3"
 
+#define PyGLM_LICENSE "PyGLM license information:\n"\
+"\n"\
+"================================================================================\n"\
+"OpenGL Mathematics (GLM)\n"\
+"--------------------------------------------------------------------------------\n"\
+"This is a redistribution of OpenGL Mathematics (GLM), which is licensed under\n"\
+"\n"\
+"================================================================================\n"\
+"The MIT License\n"\
+"--------------------------------------------------------------------------------\n"\
+"Copyright (c) 2005 G-Truc Creation\n"\
+"\n"\
+"Permission is hereby granted, free of charge, to any person obtaining a copy\n"\
+"of this software and associated documentation files (the \"Software\"), to deal\n"\
+"in the Software without restriction, including without limitation the rights\n"\
+"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"\
+"copies of the Software, and to permit persons to whom the Software is\n"\
+"furnished to do so, subject to the following conditions:\n"\
+"\n"\
+"The above copyright notice and this permission notice shall be included in\n"\
+"all copies or substantial portions of the Software.\n"\
+"\n"\
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"\
+"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"\
+"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"\
+"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"\
+"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"\
+"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n"\
+"THE SOFTWARE.\n"\
+"\n"\
+"================================================================================\n"\
+"PyGLM\n"\
+"--------------------------------------------------------------------------------\n"\
+"PyGLM is licensed under the zlib/libpng License\n"\
+"\n"\
+"================================================================================\n"\
+"zlib/libpng license\n"\
+"--------------------------------------------------------------------------------\n"\
+"Copyright (c) 2017 Zuzu_Typ\n"\
+"\n"\
+"This software is provided 'as-is', without any express or implied\n"\
+"warranty. In no event will the authors be held liable for any damages\n"\
+"arising from the use of this software.\n"\
+"\n"\
+"Permission is granted to anyone to use this software for any purpose,\n"\
+"including commercial applications, and to alter it and redistribute it\n"\
+"freely, subject to the following restrictions:\n"\
+"\n"\
+"1. The origin of this software must not be misrepresented; you must not\n"\
+"   claim that you wrote the original software. If you use this software\n"\
+"   in a product, an acknowledgment in the product documentation would be\n"\
+"   appreciated but is not required.\n"\
+"2. Altered source versions must be plainly marked as such, and must not be\n"\
+"   misrepresented as being the original software.\n"\
+"3. This notice may not be removed or altered from any source distribution."
+
+
 #define PyGLM_NO_FUNCTIONS 1
 #define PyGLM_NO_ITER_TYPECHECKING 2
 
@@ -14141,6 +14198,7 @@ PyObject* ctypes_uint8_p = NULL;
 PyObject* ctypes_bool_p = NULL;
 
 PyObject* PyGLM_VERSION_STRING = NULL;
+PyObject* PyGLM_LICENSE_STRING = NULL;
 
 #define PyGLM_FREXP_WARNING 1
 
@@ -36098,6 +36156,7 @@ static void glm_clear(PyObject*) {
 	Py_XDECREF(ctypes_cast);
 	Py_XDECREF(ctypes_void_p);
 	Py_XDECREF(PyGLM_VERSION_STRING);
+	Py_XDECREF(PyGLM_LICENSE_STRING);
 }
 
 static PyModuleDef glmmodule = {
@@ -36688,6 +36747,10 @@ extern "C" {
 		PyGLM_VERSION_STRING = PyUnicode_FromString("PyGLM (" PyGLM_BUILD_STRING ") version " PyGLM_VERSION);
 		Py_INCREF(PyGLM_VERSION_STRING);
 		PyModule_AddObject(module_glm, "version", PyGLM_VERSION_STRING);
+
+		PyGLM_LICENSE_STRING = PyUnicode_FromString(PyGLM_LICENSE);
+		Py_INCREF(PyGLM_LICENSE_STRING);
+		PyModule_AddObject(module_glm, "license", PyGLM_LICENSE_STRING);
 
 		return module_glm;
 	}
