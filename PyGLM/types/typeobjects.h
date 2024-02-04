f34bf66f14b285eb8e08374adf525c5f0e24b2c4
Zuzu-Typ <Zuzu.Typ@gmail.com>
2024-02-04 14:20:45 +0100

    (draft) debugging

--- PyGLM/types/typeobjects.h
+++ PyGLM/types/typeobjects.h
@@ -112,12 +112,15 @@ static PyGLMTypeObject PyGLMTypeObjectArray[] = {
 static PyGLMTypeObject* const PyGLMTypeObjectArrayStart = PyGLMTypeObjectArray;
 static PyGLMTypeObject* const PyGLMTypeObjectArrayEnd = reinterpret_cast<PyGLMTypeObject*>(reinterpret_cast<char*>(PyGLMTypeObjectArray) + sizeof(PyGLMTypeObjectArray));
 
+
+
 template<int L, typename T>
 static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec() {
-	if (std::is_same<T, double>::value) {
+	return 0;
+	/*if (std::is_same<T, double>::value) {
 		return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 0 + L - 1);
-	}
-	return L + sizeof(T);
+	}*/
+	
 	/*
 	if (std::is_same<T, float>::value) {
 		return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 4 + L - 1);
@@ -152,6 +155,181 @@ static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec() {
 		}
 	}*/
 }
+
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, double>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 0 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, float>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 4 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, int64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 8 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, int32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 12 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, int16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 16 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, int8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 20 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, uint64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 24 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, uint32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 28 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, uint16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 32 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, uint8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 36 + 1 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<1, bool>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 40 + 1 - 1);
+}template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, double>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 0 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, float>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 4 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, int64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 8 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, int32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 12 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, int16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 16 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, int8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 20 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, uint64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 24 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, uint32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 28 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, uint16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 32 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, uint8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 36 + 2 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<2, bool>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 40 + 2 - 1);
+}template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, double>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 0 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, float>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 4 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, int64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 8 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, int32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 12 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, int16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 16 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, int8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 20 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, uint64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 24 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, uint32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 28 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, uint16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 32 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, uint8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 36 + 3 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<3, bool>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 40 + 3 - 1);
+}template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, double>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 0 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, float>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 4 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, int64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 8 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, int32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 12 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, int16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 16 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, int8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 20 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, uint64>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 24 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, uint32>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 28 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, uint16>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 32 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, uint8>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 36 + 4 - 1);
+}
+template <>
+static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetVec<4, bool>() {
+	return static_cast<ptrdiff_t>(_PyGLM_VEC_START + 40 + 4 - 1);
+}
+
 
 template<int C, int R, typename T>
 static constexpr ptrdiff_t PyGLMTypeObjectArrayOffsetMat() {
