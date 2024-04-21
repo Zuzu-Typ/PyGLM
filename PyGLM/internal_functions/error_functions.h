0bc1534584d95f936b884f80af2c3ed2cd102788
Zuzu-Typ <Zuzu.Typ@gmail.com>
2024-04-21 13:16:24 +0200

    Update error_functions.h
    PyGLM_TYPEERROR_1O already exists as PyGLM_TYPEERROR_O

--- PyGLM/internal_functions/error_functions.h
+++ PyGLM/internal_functions/error_functions.h
@@ -4,7 +4,6 @@
 #include "../compiler_setup.h"
 
 #define PyGLM_TYPEERROR_O(str, obj) PyErr_Format(PyExc_TypeError, "%s'%s'", str, PyGLM_TYPE_AS_CSTRING(obj));
-#define PyGLM_TYPEERROR_1O(str, obj1) PyErr_Format(PyExc_TypeError, "%s'%s'", str, PyGLM_TYPE_AS_CSTRING(obj1));
 #define PyGLM_TYPEERROR_2O(str, obj1, obj2) PyErr_Format(PyExc_TypeError, "%s'%s' and '%s'", str, PyGLM_TYPE_AS_CSTRING(obj1), PyGLM_TYPE_AS_CSTRING(obj2));
 
 #define PyGLM_ZERO_DIVISION_ERROR PyErr_SetString(PyExc_ZeroDivisionError, "Whoopsie. Integers can't divide by zero (:")
