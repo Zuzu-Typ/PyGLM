e80e4d669bcf763918472a84c2c4f19f82058199
Daniel Pope <lordmauve@users.noreply.github.com>
2025-03-27 09:04:26 +0000

    Increase stack level in warnings.warn()

--- glm/__init__.py
+++ glm/__init__.py
@@ -3,4 +3,4 @@ from pyglm import glm_typing, typing, __version__
 
 import warnings as __warnings
 
-__warnings.warn("Importing PyGLM via \"import glm\" is going to be deprecated in the future.\nUse \"from pyglm import glm\" instead", PendingDeprecationWarning)
+__warnings.warn("Importing PyGLM via \"import glm\" is going to be deprecated in the future.\nUse \"from pyglm import glm\" instead", PendingDeprecationWarning, stacklevel=2)
